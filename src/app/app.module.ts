f3b9a4793e3f42ec5270bcc743505eb7a3ae61cb
Ondrej Oravcok <pista.baci@atlas.sk>
2017-09-19 09:31:42 +0200

    imports quotemarks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
-import { AppComponent } from "./app.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
